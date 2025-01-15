5022e96af223f52329072f08afe57c083c375c03
gauravchafe <gaurav.chafe@gmail.com>
2025-01-15 17:27:00 +0530

    Update theme to jekyll

---
+++ assets/js/theme.js
@@ -0,0 +1,110 @@
+const STORAGE_KEY = "theme";
+const THEME_ATTR  = "data-theme";
+const QUERY_KEY   = "(prefers-color-scheme: dark)";
+
+const themes = {
+  LIGHT: "light",
+  DARK: "dark",
+};
+
+initTheme();
+
+function initTheme() {
+  const savedTheme = localStorage.getItem(STORAGE_KEY);
+
+  if (savedTheme) {
+    // Storage theme
+    setTheme(savedTheme);
+  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+    // system theme
+    setTheme(themes.DARK);
+  } else {
+    // Default theme
+    setTheme(themes.LIGHT);
+  }
+
+  // Watch for system theme changes
+  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+    setTheme(newTheme);
+  });
+}
+
+function toggleTheme() {
+  const theme = getTheme();
+  const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
+  setTheme(newTheme);
+  localStorage.setItem(STORAGE_KEY, newTheme);
+}
+
+function getTheme() {
+  return document.documentElement.getAttribute(THEME_ATTR);
+}
+
+function setTheme(value) {
+  document.documentElement.setAttribute(THEME_ATTR, value);
+}
+
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
---
+++ pages/contact.md
@@ -0,0 +1,20 @@
+---
+layout: page
+title: Contact
+permalink: /contact/
+weight: 3
+---
+
+# **Contact Me**
+
+Hi I am **{{ site.author.name }}** :wave:,<br>
+{{ site.description }}
+
+<div class="row">
+{% include about/skills.html title="Programming Skills" source=site.data.programming-skills %}
+{% include about/skills.html title="Other Skills" source=site.data.other-skills %}
+</div>
+
+<div class="row">
+{% include about/timeline.html %}
+</div>
