f326542a7e27a81d53152c05b7c6212cee545c2e
gauravchafe <gaurav.chafe@gmail.com>
2025-01-17 16:10:24 +0530

    Layout Fixes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -44,18 +44,3 @@ function getTheme() {
 function setTheme(value) {
   document.documentElement.setAttribute(THEME_ATTR, value);
 }
-
-
-/**
- * add event on element
- */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
